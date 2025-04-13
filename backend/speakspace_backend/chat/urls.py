7a2c883eda0d2338f81af44261bd26c246ab2114
Harshil <116264438+Harshil230205@users.noreply.github.com>
2025-04-13 12:39:10 +0530

    Delete backend directory

--- backend/speakspace_backend/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('rooms/', views.RoomList.as_view(), name='room-list'),
-    path('rooms/<int:pk>/', views.RoomDetail.as_view(), name='room-detail'),
-    path('rooms/<int:room_id>/messages/', views.MessageList.as_view(), name='message-list'),
-]
--- backend/speakspace_backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserRegistrationView, UserProfileView, UserListView
-
-urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
-    path('users/', UserListView.as_view(), name='user-list'),
-]
